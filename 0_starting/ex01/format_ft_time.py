1fbbb544e2bc08d0f09443a2440dbcbbe7a5d431
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-08 18:52:41 +0400

    ex01 done

---
+++ 0_starting/ex01/format_ft_time.py
@@ -0,0 +1,11 @@
+# get the date in python
+import datetime
+dt = datetime.datetime.now()
+
+# Print Seconds since January 1, 1970: 1,666,355,857.3622 or 1.67e+09 in scientific notation$
+epoch_time = dt.timestamp()
+epoch_to_scientific = "{:.2e}".format(epoch_time)
+print("Seconds since January 1, 1970:", "{:,}".format(epoch_time), "or", epoch_to_scientific, "in scientific notation")
+
+# Print Oct 21 2022$
+print(dt.strftime("%b %d %Y"))
