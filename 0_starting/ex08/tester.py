1f3a6a97a057983fa10766ebf77af74fcf214e32
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-11 20:05:24 +0400

    ex08 prints template

--- 0_starting/ex08/tester.py
+++ 0_starting/ex08/tester.py
@@ -2,10 +2,21 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-for elem in ft_tqdm(range(333)):
-	sleep(0.005)
-print()
 
-for elem in tqdm(range(333)):
-	sleep(0.005)
-print()
+def main():
+    try:
+        # for elem in ft_tqdm(range(0, 333, 3)):
+        #     sleep(0.005)
+        # print()
+
+        # for elem2 in tqdm(range(0, 333, 3)):
+        #     sleep(0.005)
+        # print()
+    
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
