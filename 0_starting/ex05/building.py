a03fa60e149630a3f046c3df018e718f1d7c0a04
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-11 12:23:11 +0400

    finished 07 and 08

--- 0_starting/ex05/building.py
+++ 0_starting/ex05/building.py
@@ -1,50 +1,40 @@
 import sys
 
-def count_upper_case(string: str) -> int:
+def count_by_condition(string: str, condition) -> int:
+	"""Returns the count of characters based on a specific condition.
+	
+	string --  the text to evaluate
+	condition -- a function that takes a character and returns True if the character meets the condition
+	"""
+
 	count = 0
 	for c in string:
-		if (c.isupper()):
+		if condition(c):
 			count = count + 1
 	return count
 
-
-def count_lower_case(string: str) -> int:
-	count = 0
-	for c in string:
-		if (c.islower()):
-			count = count + 1
-	return count
-
-def count_punctuation(string: str) -> int:
-	count = 0
-	for c in string:
-		if c in ".,;:?!":
-			count = count + 1
-	return count
-
-def count_digits(string: str) -> int:
-	count = 0
-	for c in string:
-		if (c.isdigit()):
-			count = count + 1
-	return count
-
-def count_spaces(string: str) -> int:
-	count = 0
-	for c in string:
-		if (c.isspace()):
-			count = count + 1
-	return count
+def get_input_string() -> str:
+	if len(sys.argv) == 1:
+		print("What is the text to count?")
+		return (sys.stdin.read())
+	elif (len(sys.argv) == 2):
+		return (sys.argv[1])
+	else:
+		raise ValueError("Only one argument is allowed") 
 
 def main():
-	string = sys.argv[1]
-	print("The text contains {} characters:".format(len(string)))
+	try:
+		string = get_input_string()
 
-	print(count_upper_case(string), "upper letters")
-	print(count_lower_case(string), "lower letters")
-	print(count_punctuation(string), "punctuation marks")
-	print(count_spaces(string), "spaces")
-	print(count_digits(string), "digits")
-	
+		print("The text contains {} characters:".format(len(string)))
+		print(count_by_condition(string, lambda c: c.isupper()), "upper letters")
+		print(count_by_condition(string, lambda c: c.islower()), "lower letters")
+		print(count_by_condition(string, lambda c: c in ".,;:?!"), "punctuation marks")
+		print(count_by_condition(string, lambda c: c.isspace()), "spaces")
+		print(count_by_condition(string, lambda c: c.isdigit()), "digits")
+
+	except Exception as e:
+		print(e.__class__.__name__, ":", e)
+
 if __name__ == "__main__":
 	main()
