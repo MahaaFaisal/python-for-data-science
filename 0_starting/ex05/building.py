3dd5cd7251d6d5e21fd19086b9e5c6ac2c1cca18
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-11 20:05:24 +0400

    ex08 prints template

--- 0_starting/ex05/building.py
+++ 0_starting/ex05/building.py
@@ -1,40 +1,65 @@
 import sys
 
-def count_by_condition(string: str, condition) -> int:
-	"""Returns the count of characters based on a specific condition.
-	
-	string --  the text to evaluate
-	condition -- a function that takes a character and returns True if the character meets the condition
-	"""
 
-	count = 0
-	for c in string:
-		if condition(c):
-			count = count + 1
-	return count
+def count_by_condition(string: str, condition) -> int:
+    """Returns the count of characters based on a specific condition.
+
+    string --  the text to evaluate
+    condition -- a function that takes a character and returns True
+    if the character meets the condition
+    """
+
+    count = 0
+    for c in string:
+        if condition(c):
+            count = count + 1
+    return count
+
 
 def get_input_string() -> str:
-	if len(sys.argv) == 1:
-		print("What is the text to count?")
-		return (sys.stdin.read())
-	elif (len(sys.argv) == 2):
-		return (sys.argv[1])
-	else:
-		raise ValueError("Only one argument is allowed") 
+    if len(sys.argv) == 1:
+        print("What is the text to count?")
+        return (sys.stdin.read())
+    elif (len(sys.argv) == 2):
+        return (sys.argv[1])
+    else:
+        raise ValueError("Only one argument is allowed")
+
 
 def main():
-	try:
-		string = get_input_string()
+    try:
+        string = get_input_string()
 
-		print("The text contains {} characters:".format(len(string)))
-		print(count_by_condition(string, lambda c: c.isupper()), "upper letters")
-		print(count_by_condition(string, lambda c: c.islower()), "lower letters")
-		print(count_by_condition(string, lambda c: c in ".,;:?!"), "punctuation marks")
-		print(count_by_condition(string, lambda c: c.isspace()), "spaces")
-		print(count_by_condition(string, lambda c: c.isdigit()), "digits")
+        print("The text contains {} characters:".format(len(string)))
+        print(
+            count_by_condition(
+                string, lambda c: c.isupper()),
+            "upper letters")
 
-	except Exception as e:
-		print(e.__class__.__name__, ":", e)
+        print(
+            count_by_condition(
+                string, lambda c: c.islower()),
+            "lower letters")
+
+        print(
+            count_by_condition(
+                string, lambda c: c in ".,;:?!"),
+            "punctuation marks")
+
+        print(
+            count_by_condition(
+                string, lambda c: c.isspace()),
+            "spaces")
+
+        print(
+            count_by_condition(
+                string, lambda c: c.isdigit()),
+            "digits"
+        )
+
+    except Exception as e:
+        print(e.__class__.__name__, ":", e)
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- 0_starting/ex08/Loading.py
+++ 0_starting/ex08/Loading.py
@@ -1,2 +1,19 @@
+import os
+
+
 def ft_tqdm(lst: range) -> None:
-	
+    percentage = 0
+    fill = "█"
+    time_elapsed = "00:22"
+    etc = "02:25"
+    it_per_s = 10
+
+    print(os.get_terminal_size)
+    for idx, i in enumerate(lst):
+        print("\r{}%|{}| {}/{} [{}:{},  {}it/s]".format(
+            percentage, fill, idx, len(lst), time_elapsed, etc, it_per_s),
+            end="\r")
+        yield i
+    print("\r{}%|{}| {}/{} [{}:{},  {}it/s]".format(
+        percentage, fill, idx + 1, len(lst), time_elapsed, etc, it_per_s),
+        end="\r")
