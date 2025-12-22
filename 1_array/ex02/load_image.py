3d4ba805779d807b8a3a22576e03405dc9115330
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-22 20:15:43 +0400

    ex02 load image done, not sure of output

---
+++ 1_array/ex02/load_image.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+
+
+# (you can return to the desired format)
+def ft_load(path: str) -> np.ndarray:
+    try:
+
+        image = Image.open(path)
+        image_array = np.asarray(image)
+
+        print(f"The shape of the image is: {image_array.shape}")
+        return image_array
+
+    except OSError:
+        raise OSError("Failed to open file")
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
+    except Exception as e:
+        raise Exception(f"error loading image: {e}")
+    finally:
+        image.close()
