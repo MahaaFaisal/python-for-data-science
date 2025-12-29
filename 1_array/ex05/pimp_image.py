f9c721b676028bc4c0bf11cb377b853e6245a9cf
Maha Faisal <mafaisal@lab2r1s11.42abudhabi.ae>
2025-12-29 20:48:34 +0400

    greyscale done

--- 1_array/ex05/pimp_image.py
+++ 1_array/ex05/pimp_image.py
@@ -9,18 +9,31 @@ def ft_invert(image_arr: np.ndarray) -> np.ndarray:
 
 
 def ft_red(image_arr: np.ndarray) -> np.ndarray:
-    red_arr = image_arr[:, :, 0]
-    Image.fromarray(red_arr).show()
-    return red_arr
+    copy_arr = image_arr.copy()
+    copy_arr[:, :, 1] = copy_arr[:, :, 2] = 0
+    Image.fromarray(copy_arr).show()
+    return copy_arr
 
 
 def ft_green(image_arr: np.ndarray) -> np.ndarray:
-    return 255 - image_arr
+    copy_arr = image_arr.copy()
+    copy_arr[:, :, 0] = copy_arr[:, :, 2] = 0
+    Image.fromarray(copy_arr).show()
+    return copy_arr
 
 
 def ft_blue(image_arr: np.ndarray) -> np.ndarray:
-    return 255 - image_arr
+    copy_arr = image_arr.copy()
+    copy_arr[:, :, 0] = copy_arr[:, :, 1] = 0
+    Image.fromarray(copy_arr).show()
+    return copy_arr
 
 
 def ft_grey(image_arr: np.ndarray) -> np.ndarray:
-    return 255 - image_arr
+    copy_arr = image_arr.copy()
+
+    copy_arr[:] = np.mean(copy_arr, axis=-1, keepdims=True)
+    print(copy_arr.shape)
+
+    Image.fromarray(copy_arr).show()
+    return copy_arr
