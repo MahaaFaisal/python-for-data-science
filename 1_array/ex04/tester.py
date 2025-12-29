49ba97b02c4912cadd8d9a7d83545418e871f605
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-12-29 16:56:02 +0400

    transpose working, need to parse and add docstring

--- 1_array/ex04/tester.py
+++ 1_array/ex04/tester.py
@@ -1,5 +1,8 @@
 from load_image import ft_load
 from zoom import ft_zoom
+from rotate import ft_rotate
+from PIL import Image
+import numpy as np
 
 
 def main():
@@ -7,7 +10,9 @@ def main():
         image_arr = ft_load("animal.jpeg")
         zoomed_arr = ft_zoom(image_arr, (450, 100, 850, 500))
         print(zoomed_arr)
-        print(ft_rotate(zoomed_arr))
+        rotated_arr = ft_rotate(zoomed_arr)
+        print(rotated_arr)
+        Image.fromarray(rotated_arr).show()
     except Exception as e:
         print(e)
 
