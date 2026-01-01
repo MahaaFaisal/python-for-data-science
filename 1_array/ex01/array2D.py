20584f06ab51c0115d0cbc2864eb61bba8c8e553
mahaafaisal <mahafaisalmubarak@gmail.com>
2026-01-01 16:58:59 +0400

    ex01 parsing and docstring added

--- 1_array/ex01/array2D.py
+++ 1_array/ex01/array2D.py
@@ -2,8 +2,19 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-    family_arr = np.asarray(family)
-    print("My shape is :", np.asarray(family).shape)
+    """
+Slice a 2D list from start to end indices.
+Raises a ValueError if the input is not a proper 2D list.
+    """
+    try:
+        family_arr = np.asarray(family)
+    except Exception:
+        raise ValueError("cannot convert list to an array")
+
+    if (family_arr.ndim != 2):
+        raise ValueError("array should be 2D")
+
+    print("My shape is :", family_arr.shape)
     slice_object = slice(start, end)
     sliced_family = family[slice_object]
     print("My new shape is :", np.asarray(sliced_family).shape)
