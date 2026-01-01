2c65049f27519780bdc0f31577b653bc835c33ef
Maha Faisal <mafaisal@lab3r8s7.42abudhabi.ae>
2026-01-01 15:52:21 +0400

    ex00 parsing done

--- 1_array/ex00/tester.py
+++ 1_array/ex00/tester.py
@@ -1,4 +1,5 @@
 from give_bmi import give_bmi, apply_limit
+import sys
 
 
 def main():
@@ -7,10 +8,11 @@ def main():
         weight = [165.3, 38.4]
 
         bmi = give_bmi(height, weight)
-        print(bmi, type(bmi))
-        print(apply_limit(bmi, 26))
+        if (bmi):
+            print(bmi, type(bmi))
+            print(apply_limit(bmi, 26))
     except Exception as e:
-        print(e)
+        print(e, file=sys.stderr)
 
 
 if __name__ == "__main__":
