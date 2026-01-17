5eaff68a2248d71d580fa107aec1d80ecd388156
mahaafaisal <mahafaisalmubarak@gmail.com>
2026-01-18 02:46:29 +0400

    ex01: class method chaining successfull

---
+++ OOP/ex01/S1E7.py
@@ -0,0 +1,60 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name):
+        super().__init__(first_name)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __repr__(self):
+        return f"Vector: {(self.family_name, self.eyes, self.hairs)}"
+
+    def __str__(self):
+        return f"Vector: {(self.family_name, self.eyes, self.hairs)}"
+
+    def die(self):
+        super().die()
+
+
+class Lannister(Character):
+    """Representing the Lannister family."""
+
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __repr__(self):
+        return f"Vector: {(self.family_name, self.eyes, self.hairs)}"
+
+    def __str__(self):
+        return f"Vector: {(self.family_name, self.eyes, self.hairs)}"
+
+    def die(self):
+        super().die()
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        return cls(first_name, is_alive)
+
+
+Robert = Baratheon("Robert")
+print(Robert.__dict__)
+print(Robert.__str__)
+print(Robert.__repr__)
+print(Robert.is_alive)
+Robert.die()
+print(Robert.is_alive)
+print(Robert.__doc__)
+print("---")
+Cersei = Lannister("Cersei")
+print(Cersei.__dict__)
+print(Cersei.__str__)
+print(Cersei.is_alive)
+print("---")
+Jaine = Lannister.create_lannister("Jaine", True).create_lannister("Maha", False)
+print(f"Name : {Jaine.first_name, type(Jaine).__name__}, Alive : {Jaine.is_alive}")
