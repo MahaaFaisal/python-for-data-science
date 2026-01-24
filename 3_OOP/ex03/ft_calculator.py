586e14fce78d22009727c8155c8d93c3b7fb98e1
Maha Faisal <mahafaisalmubarak@gmail.com>
2026-01-24 13:37:11 +0400

    ex03: error handling

--- 3_OOP/ex03/ft_calculator.py
+++ 3_OOP/ex03/ft_calculator.py
@@ -4,36 +4,56 @@ class calculator:
     """a class that allows, multiplication, addition, substration\
         and division of a vector by a scaler"""
 
-    def __init__(self, object: list):
+    def __init__(self, vector: list):
         """initialization of calculator"""
-        self._list = object
+        if isinstance(vector, list) and all(
+             isinstance(x, (int, float)) for x in vector):
+            self._vector = vector
+        else:
+            print("ValueError: object should be a list of ints and floats.\
+                  initialized to []")
+            self._vector = []
 
     def __add__(self, object) -> None:
         """adds a scaler to vector element"""
-        self._list = [x + object for x in self._list]
-        print(self._list)
+        if (isinstance(object, (int, float))):
+            self._vector = [x + object for x in self._vector]
+            print(self._vector)
+        else:
+            print("ValueError: scaler should be int or float.Vector unchanged")
 
     def __mul__(self, object) -> None:
         """multiplies vector elements by a scaler"""
-        self._list = [x * object for x in self._list]
-        print(self._list)
+        if (isinstance(object, (int, float))):
+            self._vector = [x * object for x in self._vector]
+            print(self._vector)
+        else:
+            print("ValueError: scaler should be int or float.Vector unchanged")
 
     def __sub__(self, object) -> None:
         """subatracts a scaler from vector elements"""
-        self._list = [x - object for x in self._list]
-        print(self._list)
+        if (isinstance(object, (int, float))):
+            self._vector = [x - object for x in self._vector]
+            print(self._vector)
+        else:
+            print("ValueError: scaler should be int or float.Vector unchanged")
 
     def __truediv__(self, object) -> None:
         """divides vector elements by a scaler"""
         try:
-            self._list = [x / object for x in self._list]
-            print(self._list)
+            if (isinstance(object, (int, float))):
+                self._vector = [x / object for x in self._vector]
+                print(self._vector)
+            else:
+                print("ValueError: scaler should be int or float.\
+                      Vector unchanged")
+
         except ZeroDivisionError as e:
             print(f"{type(e).__name__}: {e}")
 
 
 v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-v1 + 5
+
 print("---")
 v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 v2 * 5
