2b9f7f8906b663bddc057859a8002ccfe1b8bdc9
mahaafaisal <mahafaisalmubarak@gmail.com>
2026-05-01 20:25:06 +0400

    removed nonlocal from callLimiter

--- 4_Dod/ex02/callLimit.py
+++ 4_Dod/ex02/callLimit.py
@@ -1,26 +1,27 @@
 
 def callLimit(limit: int):
     count = 0
+
     def callLimiter(function):
-        nonlocal count, limit
+
         def limit_function(*args, **kwds):
-            nonlocal count, limit
-            if count < limit:
+            nonlocal count
+            if (count < limit):
                 count += 1
-                function (*args, **kwds)
-            else:
-                print(f"Error: {function} call too many times")
+                return function(*args, **kwds)
+            print(f"Error: {function} call too many times")
         return limit_function
     return callLimiter
 
+
 @callLimit(3)
 def f():
-    print ("f()")
+    print("f()")
 
 
 @callLimit(1)
 def g():
-    print ("g()")
+    print("g()")
 
 
 for i in range(3):
